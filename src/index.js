12672626bf7d502df2047ab9b5b6568729b456ff
GenaroActis <genaroactis3@gmail.com>
2023-10-03 18:41:18 -0300

    npm start

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ root.render(
 
 export const fetchUrl = process.env.REACT_APP_FetchUrl
 export const frontUrl = process.env.REACT_APP_FrontUrl
-
+console.log('fetch', fetchUrl)
+console.log('font', frontUrl)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
